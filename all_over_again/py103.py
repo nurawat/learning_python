7013ca4323e2a52bc31de449a16006d940069b0e
nurawat <rawatarun65@gmail.com>
2020-07-10 07:44:41 +0530

    string

--- all_over_again/py103.py
+++ all_over_again/py103.py
@@ -1,1 +1,25 @@
 # Python Data Types
+# int, float, complex
+
+number = 2.345646546565
+print(type(number))
+
+
+a = 12
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a/b)
+print(a%b)
+
+# for loop
+print()
+
+for i in range(1,a//b):
+  print(i)
+
+
+print()
+
---
+++ all_over_again/strings.py
@@ -0,0 +1,19 @@
+# This is for strings
+
+name = "lemaar megussa"
+print(name)
+print("with indexing" + "  = " + name[2])
+
+# negative indexing
+print(len(name))
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-len(name)])
+print(name[-14])
+
+
+# slicing
+print(name[0:6])
+print(name[:5])
+print(name[10:9]) # no output
